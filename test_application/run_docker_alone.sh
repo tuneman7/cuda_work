7cbbc70e02829ed2aa107fc146effdd5cfda031b
don <tuneman7>
2022-08-11 21:37:32 -0700

    periodic update

--- test_application/run_docker_alone.sh
+++ test_application/run_docker_alone.sh
@@ -15,10 +15,14 @@ APP_NAME=w255_project_don_irwin_venv1
 DOCKER_FILE=Dockerfile.255project_venv1
 
 NET_NAME=w255
-echo "docker stop redis"
+
 docker stop redis
-echo "docker rm redis"
 docker rm redis
+
+echo "docker stop ${APP_NAME}"
+docker stop ${APP_NAME}
+echo "docker rm ${APP_NAME}"
+docker rm ${APP_NAME}
 
 echo "docker network rm ${NET_NAME}"
 docker network rm ${NET_NAME}
@@ -31,10 +35,36 @@ sleep 2
 
 #build docker from the docker file
 echo "docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE}"
-docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE} .
+#docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE} .
 echo "run -d --net ${NET_NAME} --name ${APP_NAME} -p 8000:8000 ${IMAGE_NAME}"
 #docker run -d --net ${NET_NAME} --name ${APP_NAME} -p 8000:8000 ${IMAGE_NAME} sleep infinity
 docker run -d --net ${NET_NAME} --name ${APP_NAME} -p 8000:8000 ${IMAGE_NAME}
+
+echo "*********************************"
+echo "*                               *"
+echo "*        WAITING. ....          *"
+echo "*        API not ready          *"
+echo "*                               *"
+echo "*********************************"
+
+
+finished=false
+while ! $finished; do
+    health_status=$(curl -o /dev/null -s -w "%{http_code}\n" -X GET "http://localhost:8000/health")
+    if [ $health_status == "200" ]; then
+        finished=true
+        echo "*********************************"
+        echo "*                               *"
+        echo "*        API is ready           *"
+        echo "*                               *"
+        echo "*********************************"
+    else
+        finished=false
+    fi
+done
+echo""
+echo""
+
 
 
 echo "**********************************"
@@ -45,8 +75,27 @@ echo "**********************************"
 
 echo "docker run -d --name redis -p 6379:6379 redis --net ${NET_NAME}"
 docker run -d --net ${NET_NAME} --name redis -p 6379:6379 redis
-echo "docker stop ${APP_NAME}"
+docker stop ${APP_NAME}
+docker rm ${APP_NAME}
+sleep 5
 docker run -d --net ${NET_NAME} --name ${APP_NAME} -p 8000:8000 ${IMAGE_NAME}
+
+finished=false
+while ! $finished; do
+    health_status=$(curl -o /dev/null -s -w "%{http_code}\n" -X GET "http://localhost:8000/health")
+    if [ $health_status == "200" ]; then
+        finished=true
+        echo "*********************************"
+        echo "*                               *"
+        echo "*        API is ready           *"
+        echo "*                               *"
+        echo "*********************************"
+    else
+        finished=false
+    fi
+done
+echo""
+echo""
 
 
 echo "**********************************"

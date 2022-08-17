b2da3d3e90e36cafabae94dc6b5efa102a949265
don <tuneman7>
2022-08-17 12:37:10 -0700

    periodic update

--- test_application/is_cuda_available.py
+++ test_application/is_cuda_available.py
@@ -2,4 +2,12 @@ import torch
 
 use_cuda = torch.cuda.is_available()
 
+if use_cuda == True:
+   print("********************")
+   print(" CUDA IS AVAILABLE  ")
+   print("********************")
+else:
+   print("*******************")
+   print(" NO CUDA AVAILABLE ")
+   PRINT("*******************")
 print(use_cuda)

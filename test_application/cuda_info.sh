b7996bba38f2bc037a753b84f36e4d22de83ced5
don <tuneman7>
2022-10-28 01:22:22 -0700

    periodic update

---
+++ test_application/cuda_info.sh
@@ -0,0 +1,1 @@
+python3 is_cuda_available.py && python3 get_cuda_info.py
--- test_application/setup_venv.sh
+++ test_application/setup_venv.sh
@@ -1,4 +1,10 @@
+deactivate
 rm -rf ./myproj
 python3 -m venv myproj
 source ./myproj/bin/activate
 . ir.sh
+
+ipython kernel install --user --name=myproj
+
+ . cuda_info.sh
+
